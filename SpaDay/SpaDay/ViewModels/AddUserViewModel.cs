bc3c27f702da7b26d28c62d7869bb9056e47ab3b
punkerdood <42493482+punkerdood@users.noreply.github.com>
2020-06-30 13:46:34 -0400

    Add files via upload
    yup

---
+++ SpaDay/SpaDay/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace SpaDay.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required(ErrorMessage = "Username is desired")]
+        [StringLength(15, MinimumLength = 5)]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is wanted")]
+        [StringLength(20, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [EmailAddress] 
+        public string Email {get; set; }
+
+
+
+        }
+}
